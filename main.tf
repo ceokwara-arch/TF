5aa2abfa0f7f959e7830ebe34357aff02cc69ca1
Meks <Meks>
2026-06-07 12:59:40 +0100

    amended file

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
 region = "eu-west-2"
 }
-resource "aws_instance" "server" {
-ami = "ami-0fb653ca2d3203ac1"
+resource "aws_instance" "example" {
+ami = "amazon_linux"
 instance_type = "t2.micro"
 }
 
