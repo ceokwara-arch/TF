0dafa941dab2aae1a27358c3047bc00f2dc47311
Meks <Meks>
2026-06-07 17:37:03 +0100

    made changes

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
 provider "aws" {
-  region = "eu-west-2"
+region = "us-east-2"
 }
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-}
-
-resource "aws_instance" "my-instance" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+resource "aws_instance" "example" {
+ami = "ami-0fb653ca2d3203ac1"
+8
+instance_type = "t2.micro"
 }
