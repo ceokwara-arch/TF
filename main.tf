2e7f59ac1aa8745891bd2ff2bce7eb46eb5d5d7e
Meks <Meks>
2026-06-07 17:35:56 +0100

    made changes

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 provider "aws" {
-region = "eu-west-2"
+  region = "eu-west-2"
 }
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
 resource "aws_instance" "my-instance" {
-ami = data.external.latest_ami.result.ImageId
-instance_type = "t2.micro"
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
 }
